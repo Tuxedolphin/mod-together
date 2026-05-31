a832068a40095a7f14ba8f057ff679b3ff0c4685
Jordan <jyodann@outlook.com>
2026-05-31 15:00:10 +0800

    Saved with Prettier

--- frontend/src/lib/types/modules.ts
+++ frontend/src/lib/types/modules.ts
@@ -17,41 +17,41 @@ export type Workload = string | Array<number>;
 export type Venue = string;
 
 export type WeekRange = {
-  end: string;
-  // The start and end dates
-  start: string;
-  // Number of weeks between each lesson. If not specified one week is assumed
-  // ie. there are lessons every week
-  weekInterval?: number;
-  // Week numbers for modules with uneven spacing between lessons. The first
-  // occurrence is on week 1
-  weeks?: Array<number>;
+	end: string;
+	// The start and end dates
+	start: string;
+	// Number of weeks between each lesson. If not specified one week is assumed
+	// ie. there are lessons every week
+	weekInterval?: number;
+	// Week numbers for modules with uneven spacing between lessons. The first
+	// occurrence is on week 1
+	weeks?: Array<number>;
 };
 
 export type Weeks = Array<number> | WeekRange;
 
 // Recursive tree of module codes and boolean operators for the prereq tree
 export type PrereqTree =
-  | string
-  | { and?: Array<PrereqTree>; nOf?: [number, Array<PrereqTree>]; or?: Array<PrereqTree> };
+	| string
+	| { and?: Array<PrereqTree>; nOf?: [number, Array<PrereqTree>]; or?: Array<PrereqTree> };
 
 // Auxiliary data types
 export type Day =
-  | 'Monday'
-  | 'Tuesday'
-  | 'Wednesday'
-  | 'Thursday'
-  | 'Friday'
-  | 'Saturday'
-  | 'Sunday';
+	| 'Monday'
+	| 'Tuesday'
+	| 'Wednesday'
+	| 'Thursday'
+	| 'Friday'
+	| 'Saturday'
+	| 'Sunday';
 
 export const WorkingDaysOfWeek: Array<Day> = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
 ];
 
 export const DaysOfWeek: Array<Day> = [...WorkingDaysOfWeek, 'Sunday'];
@@ -67,114 +67,114 @@ export type WorkloadComponent = 'Lecture' | 'Tutorial' | 'Laboratory' | 'Project
 // RawLesson is a lesson time slot obtained from the API.
 // Usually ModuleCode and ModuleTitle has to be injected in before using in the timetable.
 export type RawLesson = Readonly<{
-  classNo: ClassNo;
-  day: DayText;
-  endTime: EndTime;
-  lessonType: LessonType;
-  size: number;
-  startTime: StartTime;
-  venue: Venue;
-  weeks: Weeks;
+	classNo: ClassNo;
+	day: DayText;
+	endTime: EndTime;
+	lessonType: LessonType;
+	size: number;
+	startTime: StartTime;
+	venue: Venue;
+	weeks: Weeks;
 }>;
 
 // Semester-specific information of a module.
 export type SemesterData = {
-  // Aggregated from timetable
-  // Exam
-  examDate?: string;
-  examDuration?: number;
-  semester: Semester;
-  timetable: Array<RawLesson>;
+	// Aggregated from timetable
+	// Exam
+	examDate?: string;
+	examDuration?: number;
+	semester: Semester;
+	timetable: Array<RawLesson>;
 };
 
 export type NUSModuleAttributes = Partial<{
-  fyp: boolean; // Honours / Final Year Project
-  grsu: boolean; // Can S/U (graduate)
-  ism: boolean; // Independent study
-  lab: boolean; // Lab based
-  mpes1: boolean; // Included in Semester 1's Module Planning Exercise
-  mpes2: boolean; // Included in Semester 2's Module Planning Exercise
-  sfs: boolean; // SkillsFuture series
-  ssgf: boolean; // SkillsFuture Funded
-  su: boolean; // Can S/U (undergraduate)
-  urop: boolean; // Undergraduate Research Opportunities Program
-  year: boolean; // Year long
+	fyp: boolean; // Honours / Final Year Project
+	grsu: boolean; // Can S/U (graduate)
+	ism: boolean; // Independent study
+	lab: boolean; // Lab based
+	mpes1: boolean; // Included in Semester 1's Module Planning Exercise
+	mpes2: boolean; // Included in Semester 2's Module Planning Exercise
+	sfs: boolean; // SkillsFuture series
+	ssgf: boolean; // SkillsFuture Funded
+	su: boolean; // Can S/U (undergraduate)
+	urop: boolean; // Undergraduate Research Opportunities Program
+	year: boolean; // Year long
 }>;
 
 // Information for a module for a particular academic year.
 export type Module = {
-  acadYear: AcadYear;
+	acadYear: AcadYear;
 
-  additionalInformation?: string;
-  aliases?: Array<ModuleCode>;
+	additionalInformation?: string;
+	aliases?: Array<ModuleCode>;
 
-  attributes?: NUSModuleAttributes;
-  corequisite?: string;
-  corequisiteRule?: string;
-  department: Department;
-  // Additional info
-  description?: string;
-  faculty: Faculty;
-  fulfillRequirements?: Array<ModuleCode>;
-  gradingBasisDescription?: string;
-  // Basic info
-  moduleCode: ModuleCode;
+	attributes?: NUSModuleAttributes;
+	corequisite?: string;
+	corequisiteRule?: string;
+	department: Department;
+	// Additional info
+	description?: string;
+	faculty: Faculty;
+	fulfillRequirements?: Array<ModuleCode>;
+	gradingBasisDescription?: string;
+	// Basic info
+	moduleCode: ModuleCode;
 
-  moduleCredit: string;
-  preclusion?: string;
-  preclusionRule?: string;
-  // Requisite tree
-  prereqTree?: PrereqTree;
-  // Requsites
-  prerequisite?: string;
-  prerequisiteAdvisory?: string;
-  prerequisiteRule?: string;
+	moduleCredit: string;
+	preclusion?: string;
+	preclusionRule?: string;
+	// Requisite tree
+	prereqTree?: PrereqTree;
+	// Requsites
+	prerequisite?: string;
+	prerequisiteAdvisory?: string;
+	prerequisiteRule?: string;
 
-  // Semester data
-  semesterData: Array<SemesterData>;
+	// Semester data
+	semesterData: Array<SemesterData>;
 
-  title: ModuleTitle;
-  workload?: Workload;
+	title: ModuleTitle;
+	workload?: Workload;
 };
 
 // This format is returned from the module list endpoint.
 export type ModuleCondensed = Readonly<{
-  moduleCode: ModuleCode;
-  semesters: Array<number>;
-  title: ModuleTitle;
+	moduleCode: ModuleCode;
+	semesters: Array<number>;
+	title: ModuleTitle;
 }>;
 
 // This format is returned from the module information endpoint
 export type SemesterDataCondensed = Readonly<
-  // The full timetable is omitted to save space
-  Omit<SemesterData, 'timetable'>
+	// The full timetable is omitted to save space
+	Omit<SemesterData, 'timetable'>
 >;
 
 export type ModuleInformation = Readonly<{
-  attributes?: NUSModuleAttributes;
-  corequisite?: string;
+	attributes?: NUSModuleAttributes;
+	corequisite?: string;
 
-  department: Department;
-  // Additional info
-  description?: string;
-  faculty: Faculty;
-  gradingBasisDescription?: string;
-  // Basic info
-  moduleCode: ModuleCode;
-  moduleCredit: string;
-  preclusion?: string;
+	department: Department;
+	// Additional info
+	description?: string;
+	faculty: Faculty;
+	gradingBasisDescription?: string;
+	// Basic info
+	moduleCode: ModuleCode;
+	moduleCredit: string;
+	preclusion?: string;
 
-  // Requsites
-  prerequisite?: string;
-  // Condensed semester info
-  semesterData: Array<SemesterDataCondensed>;
-  title: ModuleTitle;
+	// Requsites
+	prerequisite?: string;
+	// Condensed semester info
+	semesterData: Array<SemesterDataCondensed>;
+	title: ModuleTitle;
 
-  workload?: Workload;
+	workload?: Workload;
 
-  // Requisite tree is not returned to save space
+	// Requisite tree is not returned to save space
 }>;
 
 export type Aliases = {
-  [moduleCode: string]: Array<ModuleCode>;
+	[moduleCode: string]: Array<ModuleCode>;
 };
