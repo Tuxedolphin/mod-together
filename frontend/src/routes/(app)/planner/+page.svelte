4917ddaaf008acd0d19e455d2dddcc555902a839
Jordan <jyodann@outlook.com>
2026-07-23 12:45:54 +0800

    Hotfix Prod

--- frontend/src/routes/(app)/planner/+page.svelte
+++ frontend/src/routes/(app)/planner/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-  import type { PageProps } from "./$types";
+  import AvailableTimetableGrid from "$lib/components/AvailableTimetableGrid.svelte";
 </script>
 
 <p class="mt-2 text-xl font-semibold">Select a timetable to start planning:</p>
 <hr class="my-2 h-px border-0 bg-gray-200" />
+
+<AvailableTimetableGrid></AvailableTimetableGrid>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-	import LoginComponent from '$lib/components/LoginPage/LoginComponent.svelte';
-	import ModTogetherHero from '$lib/components/LoginPage/ModTogetherHero.svelte';
+  import LoginComponent from "$lib/components/LoginPage/LoginComponent.svelte";
+  import ModTogetherHero from "$lib/components/LoginPage/ModTogetherHero.svelte";
 </script>
 
 <ModTogetherHero>
-	<LoginComponent></LoginComponent>
-	<!-- <div class="divider">OR</div> -->
-	<!-- <GuestTryoutComponent></GuestTryoutComponent> -->
+  <div class="alert alert-error alert-soft text-center">
+    <span>⚠️ </span>
+    Mods Together is in Beta. Timetable information might be lost during certain
+    version changes. Remember to always keep a safe copy with you. Happy Modding!
+  </div>
+
+  <div class="py-2">
+    <div class="text-info-content bg-info p-2 rounded-2xl">
+      You may report any issues <a
+        class="underline"
+        href="https://forms.gle/Aggf9kMYjaLchtrPA">here</a
+      >
+    </div>
+  </div>
+  <LoginComponent></LoginComponent>
+  <!-- <div class="divider">OR</div> -->
+  <!-- <GuestTryoutComponent></GuestTryoutComponent> -->
 </ModTogetherHero>
